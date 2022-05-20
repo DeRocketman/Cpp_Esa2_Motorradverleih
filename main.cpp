4fe36e364edac5c704444f98e7092e2821080718
DeRocketman <de.rocketman@outlook.com>
2022-05-20 15:16:09 +0200

    add first classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
+class Address {
+    string street;
+    string number;
+    string zip;
+    string city;
+    string country;
+
+};
+
+class Customer {
+private:
+    string name;
+    string firstName;
+    Address address;
+    int yearBirth;
+    string phone;
+    bool hasLicence;
+
+public:
+};
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
