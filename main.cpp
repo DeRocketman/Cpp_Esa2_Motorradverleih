ee2906102f2a3d7c1b5e4f5bf99120141307c09b
stricker <de.rocketman@outlook.com>
2022-05-21 23:53:51 +0200

    Add classes

--- main.cpp
+++ main.cpp
@@ -22,6 +22,32 @@ private:
 
 public:
 };
+
+class Date {
+private:
+    int year;
+    int month;
+    int day;
+public:
+    Date(int y, int m, int d): year(y), month(m), day(d) {};
+};
+
+class Bike {
+private:
+    string registrationNumber;
+    string type;
+public:
+
+};
+class Reservation {
+private:
+    int id;
+    Date rentalStart;
+    Date rentalEnd;
+    Customer customer;
+    Bike bike;
+public:
+};
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
