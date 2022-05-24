48d9386f92775510b1246c404bf7054fda30f767
stricker <de.rocketman@outlook.com>
2022-05-24 22:34:46 +0200

    Add functions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 class Address {
+private:
     string street;
     string number;
     string zip;
@@ -22,9 +23,12 @@ private:
     string phone;
     bool hasLicence;
 public:
-
     friend istream& operator>>(istream& is, Customer& customer);
     friend ostream& operator<<(ostream& os, const Customer& customer);
+
+    string getName() const {
+        return name;
+    }
 };
 
 istream &operator>>(istream& is, Customer &customer) {
@@ -42,6 +46,18 @@ private:
     int day;
 public:
     Date(int y, int m, int d): year(y), month(m), day(d) {};
+
+    int getDay() const {
+        return day;
+    }
+
+    int getMonth() const {
+        return month;
+    }
+
+    int getYear() const {
+        return year;
+    }
 };
 
 class Bike {
@@ -52,9 +68,18 @@ public:
     Bike(string reg, string type) : registrationNumber(reg), type(type) {};
     friend istream& operator>>(istream& is, Bike& bike);
     friend ostream& operator<<(ostream& os, const Bike& bike);
+
+    string getType() const {
+        return type;
+    }
 };
 
 istream &operator>>(istream& is, Bike &bike) {
+    cout << "Bitte geben Sie die Daten für das Motorrad ein: " << endl;
+    cout << endl << "Kennzeichen: ";
+    is >> bike.registrationNumber;
+    cout << "Type: ";
+    is >> bike.type;
     return is;
 }
 
@@ -71,8 +96,21 @@ private:
     Customer customer;
     Bike bike;
 public:
+    friend istream& operator>>(istream& is, Reservation& reservation);
+    friend ostream& operator<<(ostream& os, const Reservation& reservation);
 };
 
+istream &operator>>(istream& is, Reservation &reservation) {
+    return is;
+}
+
+ostream& operator<<(ostream& os, const Reservation& reservation) {
+    os << reservation.id << " "
+    << reservation.rentalStart.getDay()<< "." << reservation.rentalStart.getMonth() << "." << reservation.rentalStart.getYear() << " "
+    << reservation.rentalEnd.getDay()<< "." << reservation.rentalEnd.getMonth() << "." << reservation.rentalEnd.getYear() << " "
+    << reservation.customer.getName()<< " " << endl;
+    return os;
+}
 class BikeRental {
 private:
     list<Customer> customers;
@@ -81,9 +119,10 @@ private:
 public:
     void showMainMenu();
     void readDataFromFile(int fileId);
-    void writeDataToFile();
+    void writeDataToFile(int fileId);
     void showCustomerManagement();
     void showReservationManagement();
+    void showBikeManagement();
     void createNewCustomer();
     void editCustomerData();
     void deleteCustomerData();
@@ -96,6 +135,7 @@ void BikeRental::showMainMenu() {
         cout << "----------MEN\x9A \b-----------" << endl;
         cout << "[1] Kundinnenverwaltung" << endl;
         cout << "[2] Reservierungen" << endl;
+        cout << "[3] Motorradverwaltung" << endl;
         cout << "[0] Beenden" << endl;
         cout << "--------------------------" << endl;
         cout << endl << "Ihre Auswahl:";
@@ -105,8 +145,11 @@ void BikeRental::showMainMenu() {
             showCustomerManagement();
         } else if (choice==2) {
             showReservationManagement();
+        } else if (choice==3) {
+            showBikeManagement();
         } else if (choice==0) {
-            cout << "Programm wird beendet" << endl;
+                cout << "Programm wird beendet" << endl;
+
         }
     }
 }
@@ -138,13 +181,33 @@ void BikeRental::readDataFromFile(int fileId) {
                     writeDemoBike << i;
                 }
             }
+        } else {
+
         }
     } else {
 
     }
 }
 
-void BikeRental::writeDataToFile() {}
+void BikeRental::writeDataToFile(int fileId) {
+    ofstream writeReservationFile("reservierungen.txt");
+    ofstream writeBikeFile("bikes.txt");
+    ofstream writeCustomerFile("kunden.txt");
+
+    if (fileId==1) {
+        for (const auto& reservation: reservations) {
+            writeReservationFile << reservation;
+        }
+    } else if (fileId==2) {
+        for (const auto& bike: bikes) {
+            writeBikeFile << bike;
+        }
+    } else {
+        for (const auto& customer: customers) {
+            writeCustomerFile << customer;
+        }
+    }
+}
 
 void BikeRental::showCustomerManagement() {
     int choice=5;
@@ -183,6 +246,8 @@ void BikeRental::deleteCustomerData() {}
 void BikeRental::showAllCustomersData() {}
 
 void BikeRental::showReservationManagement() {}
+
+void BikeRental::showBikeManagement() {}
 
 int main() {
     BikeRental bikeRental;
